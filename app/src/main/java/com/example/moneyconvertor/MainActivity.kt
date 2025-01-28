44faf10508fcb76a0696bb4e5c38022d854651f7
Mohammad Nawas <mohammednawas8@gmail.com>
2025-01-28 19:43:06 +0200

    Testing API Key On bitrise VM

--- app/src/main/java/com/example/moneyconvertor/MainActivity.kt
+++ app/src/main/java/com/example/moneyconvertor/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.moneyconvertor.navigation.MoneyConvertorNavGraph
 import com.mc.designsystem.theme.MoneyConvertorTheme
+import com.mc.network.BuildConfig
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -17,7 +23,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MoneyConvertorTheme {
-                MoneyConvertorNavGraph()
+//                MoneyConvertorNavGraph()
+
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text("ApiKey: ${BuildConfig.apiKey}")
+                }
             }
         }
     }
