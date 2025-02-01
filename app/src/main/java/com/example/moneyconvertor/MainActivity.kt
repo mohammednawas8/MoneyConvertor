35e276e04aaa5ce7ed8679c242e64ecc96295d8a
Mohammad Nawas <mohammednawas8@gmail.com>
2025-02-01 17:04:51 +0200

    Update design system theme and add core-ktx dependency

--- app/src/main/java/com/example/moneyconvertor/MainActivity.kt
+++ app/src/main/java/com/example/moneyconvertor/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.example.moneyconvertor.navigation.MoneyConvertorNavGraph
 import com.mc.designsystem.theme.MoneyConvertorTheme
-import com.mc.network.BuildConfig
 import dagger.hilt.android.AndroidEntryPoint
 
 
